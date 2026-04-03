0e9a4242e4a93e901bdee79266554d55653f5926
Yash <209734246+Yash-aga-18@users.noreply.github.com>
2026-04-03 16:13:20 +0530

    basic file is updated

--- 27 Map & set/1 sets/basics.cpp
+++ 27 Map & set/1 sets/basics.cpp
@@ -3,40 +3,49 @@ using namespace std;
 
 int main()
 {
+    // =========================
+    // UNORDERED SET (Hash Table)
+    // =========================
+
     unordered_set<int> s;
 
+    // INSERTION (Average O(1))
     s.insert(10);
     s.insert(20);
     s.insert(30);
     s.insert(40);
     s.insert(50);
-    
-    cout<<s.size() <<endl;
-    for(int ele :s)     cout<<ele <<" ";
-    cout<<endl;
 
-    // we canmt insert the duplicate elemnts
-    
-    s.insert(40);
-    s.insert(40);
-    s.insert(40);
+    // Traversal (order is NOT sorted, depends on hashing)
+    cout << s.size() << endl;
+    for(int ele : s)     cout << ele << " ";
+    cout << endl;
+
+    // Duplicate elements are NOT allowed
+    s.insert(40);  // ignored
+    s.insert(40);  // ignored
+    s.insert(40);  // ignored
+
     s.insert(100);
     s.insert(200);
-    
+
+    // DELETE operation (Average O(1))
     s.erase(30);
 
-    cout<<s.size() <<endl;
-    for(int ele :s)     cout<<ele <<" ";
-    cout<<endl;
-    
+    cout << s.size() << endl;
+    for(int ele : s)     cout << ele << " ";
+    cout << endl;
+
+    // SEARCH operation (Average O(1))
     int target = 30;
-    if( s.find(target) != s.end()  ){
-        cout<<"exist"<<endl;
-    }
-    else    cout<<"not exist"<<endl;
-    // s.find(is used to find the target till last  and if not found it returns last elemnt)
 
+    if( s.find(target) != s.end() )   cout << "exist" << endl;
+    else                            cout << "not exist" << endl;
     
- return 0;
 
+    // NOTE:
+    // s.find(x) returns iterator to element if found
+    // otherwise returns s.end()
+
+    return 0;
 }
